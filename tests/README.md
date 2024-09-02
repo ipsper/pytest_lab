656c15e09ca6c08d0cbcd4f2c6ae9f7e3ee272b7
pnehlin <per.nehlin@ip-solutions.se>
2024-09-02 12:24:18 +0200

    labba med att printa docktext i rapport

--- tests/README.md
+++ tests/README.md
@@ -3,3 +3,16 @@
 ```
 python -m pytest -sx tests/test_of_localhost.py
 ```
+## Run tests with pytest-html
+```
+python -m pytest --html=rapport_of_localhost.html -sx tests/test_of_localhost.py
+```
+### review
+- helt lite tunnt ingen dock tag saknade historik vid första 
+
+## Run tests with pytest-html
+```
+python -m pytest --html=rapport_of_localhost.html -sx tests/test_of_localhost.py
+```
+### review
+- helt lite tunnt ingen dock tag saknade historik vid första 
--- tests/test_of_localhost.py
+++ tests/test_of_localhost.py
@@ -2,13 +2,24 @@
 import time
 from tools import pip_api
 
-def test_pip_package_version():
-    """Test of get loggs api"""
+def test_pip_package_requests_version():
+    """Test that the pip package requests version"""
     start_time = time.time()
     package_name = "requests"
     package_version = pip_api.get_package_version(package_name)
-    print(f"test_pip_package_version {package_version} version")
+    print(f"test_pip_package_requests_version {package_version} version")
     end_time = time.time()
     runtime = end_time - start_time
-    print(f"runtime for test_pip_package_version {runtime} seconds")
+    print(f"runtime for test_pip_package_requests_version {runtime} seconds")
+    assert package_version >= "2.28.1"
+
+def test_pip_package_urllib3_version():
+    """Test that the pip package urllib3 version"""
+    start_time = time.time()
+    package_name = "urllib3"
+    package_version = pip_api.get_package_version(package_name)
+    print(f"test_pip_package_urllib3_version {package_version} version")
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"runtime for test_pip_package_urllib3_version {runtime} seconds")
     assert package_version >= "2.28.1"
